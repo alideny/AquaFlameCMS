69a841a2ffa319c4105ca6316b9f9526c1e84869
unknown <Yannick@FrickX.(none)>
2011-12-23 17:35:11 +0100

    Config-File = OOP

--- configs.php
+++ configs.php
@@ -56,57 +56,60 @@ switch ($lang)
         include ("lang/en-us.php");
         $lang = 'en-us';
 }
-/*||||||||||||||||||||||||||||||||||||||||*/
-/*||||||||The Server Configuration||||||||*/
-/*||||||||||||||||||||||||||||||||||||||||*/
-$serveraddress = "localhost"; // Your MySQL server address
-$serveruser = "root"; // Your MySQL user
-$serverpass = "ascent"; //Your MySQL password
-$serverport	= "3306"; // Your MySQL Port
-$donatadmin = "ascent"; // Your Donation Admin Password
-$website['realm'] = "Set Realmlist Your_Realmlist"; // You realm (Example: Set Realmlist login.wowfailure.com)
-/*||||||||||||||||||||||||||||||||||||*/
-/*|||||||The Admin Configuration||||||*/
-/*||||||||||||||||||||||||||||||||||||*/
-// define('MYSQL_SERVER', 'localhost'); // Your MySQL server address
-// define('MYSQL_USERNAME', 'root'); // Your MySQL user
-// define('MYSQL_PASSWORD', 'ascent'); //Your MySQL password
-// define('MYSQL_DATABASE', 'site'); //Your website database
-// Leave it True if you want to Manage only the Website. Change it to false if you want to manage all your Databases
-// define('MYSQL_PREDEFINED_DATABASE', TRUE); # if set to true, we use MYSQL_DATABASE as the database, otherwise user can select which database to use
-/*||||||||||||||||||||||||||||||||||||||||*/
-/*|||||||The Website Configuration||||||||*/
-/*||||||||||||||||||||||||||||||||||||||||*/
-$server_db = "site"; //Your website database
-$server_adb = "auth"; //Your account database
-$server_cdb = "characters"; //Your characters database
-$server_cdb_2 = "characters"; //Your characters database
-$server_wdb = "world"; //Your world database
-$server_wdb_2 = "world"; //Your world database
-$charTable = 'characters'; //Your Character Table!!
-$name_realm1['realm'] = "Server_Name_1"; //Your Realm Name 1
-$name_realm2['realm'] = "Server_Name_2"; //Your Realm Name 2
-$realm_count = '1'; //number of realms
-$charLimit = '10'; //Max characters to show on the page
-$mysql_cod = 'cp1251';
-$website['title'] = "WoWFailureCMS"; // Your Website Title
-$website['slogan'] = "WoWFailureCMS, get your best CMS today, simple and fast!"; // Your Website Slogan.
-$website['address'] = "http://www.wowfailure.co.cc"; // 'http://url/foldername/' or 'http://url/'
-$website['root'] = "/WoWFailureCMS/"; // '/' <- for root and '/foldername/' <- for any folder
-/*||||||||||||||||||||||||||||||||||||||||*/
-/*||||||||||||DO NOT EDIT BELOW|||||||||||*/
-/*||||||||||||||||||||||||||||||||||||||||*/
-/*||||||||||||DO NOT EDIT BELOW|||||||||||*/
-/*||||||||||||||||||||||||||||||||||||||||*/
-$connection_setup = mysql_connect($serveraddress,$serveruser,$serverpass)or die(mysql_error());
-mysql_select_db($server_db,$connection_setup)or die(mysql_error());
 
-if(isset($_SESSION['username'])){
-	mysql_select_db($server_adb,$connection_setup)or die(mysql_error());
-	$username = mysql_real_escape_string($_SESSION['username']);
-	$lbrspa = mysql_query("SELECT * FROM account WHERE username = '".$username."'");
-	$account_information = mysql_fetch_assoc($lbrspa);
-	mysql_select_db($server_db,$connection_setup)or die(mysql_error());
+class Configuration
+{
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    /*||||||||The Server Configuration||||||||*/
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    public $serveraddress = "localhost"; // Your MySQL server address
+    public $serveruser = "root"; // Your MySQL user
+    public $serverpass = "ascent"; //Your MySQL password
+    public $serverport = "3306"; // Your MySQL Port
+    public $donatadmin = "ascent"; // Your Donation Admin Password
+    public $website['realm'] = "Set Realmlist Your_Realmlist"; // You realm (Example: Set Realmlist login.wowfailure.com)
+
+
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    /*|||||||The Website Configuration||||||||*/
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    public $server_db = "site"; //Your website database
+    public $server_adb = "auth"; //Your account database
+    public $server_cdb = "characters"; //Your characters database
+    public $server_cdb_2 = "characters"; //Your characters database
+    public $server_wdb = "world"; //Your world database
+    public $server_wdb_2 = "world"; //Your world database
+    public $charTable = 'characters'; //Your Character Table!!
+    public $name_realm1['realm'] = "Server_Name_1"; //Your Realm Name 1
+    public $name_realm2['realm'] = "Server_Name_2"; //Your Realm Name 2private public $
+    public $realm_count = '1'; //number of realmsprivate public $
+    public $charLimit = '10'; //Max characters to show on the pageprivate public $
+    public $mysql_cod = 'cp1251';
+    public $website['title'] = "WoWFailureCMS"; // Your Website Title
+    public $website['slogan'] = "WoWFailureCMS, get your best CMS today, simple and fast!"; // Your Website Slogan.
+    public $website['address'] = "http://www.wowfailure.co.cc"; // 'http://url/foldername/' or 'http://url/'
+    public $website['root'] = "/WoWFailureCMS/"; // '/' <- for root and '/foldername/' <- for any folder
 }
+
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    /*||||||||||||DO NOT EDIT BELOW|||||||||||*/
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    /*||||||||||||DO NOT EDIT BELOW|||||||||||*/
+    /*||||||||||||||||||||||||||||||||||||||||*/
+    $connection_setup = mysql_connect($serveraddress,$serveruser,$serverpass)or die(mysql_error());
+    mysql_select_db($server_db,$connection_setup)or die(mysql_error());
+
+    /*
+    We don't need it, because it doesn't have an effect!
+
+    if(isset($_SESSION['username']))
+    {
+        mysql_select_db($server_adb,$connection_setup)or die(mysql_error());
+        $username = mysql_real_escape_string($_SESSION['username']);
+        $lbrspa = mysql_query("SELECT * FROM account WHERE username = '".$username."'");
+        $account_information = mysql_fetch_assoc($lbrspa);
+        mysql_select_db($server_db,$connection_setup)or die(mysql_error());
+    }
+    */
 ?>
 <script type="text/javascript" src="http://static.wowhead.com/widgets/power.js"></script>
